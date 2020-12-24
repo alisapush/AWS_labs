00df7aa95d0c9def5225e460c405903849842189
Alisa Pushnova <alisa.pushnova@gmail.com>
2020-12-24 13:14:10 +0300

    add the 3th lab

--- AWS_Lab1.cpp
+++ AWS_Lab1.cpp
@@ -1,86 +1,231 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <iostream>
+#include <iomanip> 
 using namespace std;
 
-// вариант 21) F[i]=A[i]-(B[i]*C[i])+D[i] , i=1...8;
+// вариант 21) F[i] = A[i] - (B[i] * C[i]) + D[i] , i = 1...8;
 
-int main() 
+int main()
 {
-	_int8  a[8] = { 16, 24, 6, 98, 12, 29, 78, 12 };
-	_int8  b[8] = { 3, -4, 10, 10, -3, -4, 11, 0 };
-	_int8  c[8] = { 2, -5, -3, 10, -2, -5, 12, 27 };
-	_int16 d[8] = { -2, 1, -33, 7, -6, -8, 56, -9 };
-	_int16 f[8];
+	_int8  A[8] = { 15, 24, 6, -98, 12, 29, 78, 12 };
+	_int8  B[8] = { 3, -4, 10, 10, -3, -4, 11, 0 };
+	_int8  C[8] = { 2, -5, -3, 10, -2, -5, 12, 27 };
+	_int16 D[8] = { 1, 1, -33, 7, -6, -8, 56, -9 };
+	_int16 F[8];
 
-	_asm {
+	_asm
+	{
+		// Init all arrays
+		movq mm0, qword ptr[A]
+		movq mm1, qword ptr[B]
+		movq mm2, qword ptr[C]
 
-		// расширение
-		// регистры для ускорения производиельности
+		// F[i] = A[i] - (B[i]*C[i]) + D[i]
 
-		movq mm0, qword ptr[a] // регист 64 разряда кладу массив а
-		movq mm1, qword ptr[b]
-		movq mm2, qword ptr[c]
-		movq mm3, qword ptr[d]
 
-		// скрешивание значений
 
-		punpcklbw mm0, mm0
-		punpcklbw mm1, mm1
-		punpcklbw mm2, mm2
 
-		// сдвиг каждой ячейки и добавляем нули
+		// UNPACK B
+		// Copy B to temp mm4
+		movq mm4, mm1
 
-		psraw     mm0, 8
-		psraw     mm1, 8
-		psraw     mm2, 8
+		// Zero-ing temp mm3
+		psubsw mm3, mm3
 
-		// в 64 бита влазит четыре 16 битных элемента
+		// Unpacking mm4 from 8 chars to 4 words
+		pcmpgtb mm3, mm4
+		// Low-order bytes (Lbw)
+		punpcklbw mm4, mm3
 
-		pmullw mm1, mm2
-		psubsw mm0, mm1
-		paddsw mm0, mm3
+		// Place result to F1
+		movq mm5, mm4
 
-		// осталось обработать 4 элемента во второй половине
 
-		movq qword ptr[f], mm0
 
-		movq mm0, qword ptr[a + 4]
-		movq mm1, qword ptr[b + 4]
-		movq mm2, qword ptr[c + 4]
-		movq mm3, qword ptr[d + 8]
 
-		punpcklbw mm0, mm0
-		punpcklbw mm1, mm1
-		punpcklbw mm2, mm2
+		// The same with C
+		// Copy C to temp mm4
+		movq mm4, mm2
 
-		psraw     mm0, 8
-		psraw     mm1, 8
-		psraw     mm2, 8
+		// Zero-ing temp mm3
+		psubsw mm3, mm3
 
-		pmullw mm1, mm2
-		psubsw mm0, mm1
-		paddsw mm0, mm3
+		// Unpacking mm4 from 8 chars to 4 words
+		pcmpgtb mm3, mm4
+		// Low-order bytes (Lbw)
+		punpcklbw mm4, mm3
 
-		movq qword ptr[f + 8], mm0
+		// Place result to temp mm7
+		movq mm7, mm4
+	
+
+
+
+		// B * C (word * word)
+		pmullw mm5, mm7
+
+
+
+
+		// Make A as word (2 bytes each item)
+		movq mm4, mm0
+
+		// Zero-ing temp mm3
+		psubsw mm3, mm3
+
+		// Unpacking mm4 from 8 chars to 4 words
+		pcmpgtb mm3, mm4
+		// Low-order bytes (Lbw)
+		punpcklbw mm4, mm3
+
+		// Place result to temp mm7
+		movq mm7, mm4
+
+
+
+		// A --- B*C
+		psubsw mm7, mm5
+		movq mm5, mm7
+
+
+
+
+
+		// A-B*C +++ D
+		paddsw mm5, qword ptr[D]
+
+
+		// =================================================================================
+
+
+
+
+		// UNPACK B
+		// Copy B to temp mm4
+		movq mm4, mm1
+
+		// Zero-ing temp mm3
+		psubsw mm3, mm3
+
+		// Unpacking mm4 from 8 chars to 4 words
+		pcmpgtb mm3, mm4
+		// High-order bytes (Hbw)
+		punpckhbw mm4, mm3
+
+		// Place result to F2 (second pard of F stored in mm6)
+		movq mm6, mm4
+
+
+
+
+
+		// The same with C
+		// Copy C to temp mm4
+		movq mm4, mm2
+
+		// Zero-ing temp mm3
+		psubsw mm3, mm3
+
+		// Unpacking mm4 from 8 chars to 4 words
+		pcmpgtb mm3, mm4
+		// High-order bytes (Hbw)
+		punpckhbw mm4, mm3
+
+		// Place result to temp mm7
+		movq mm7, mm4
+
+
+
+
+		// B * C (word * word)
+		pmullw mm6, mm7
+
+
+
+
+		// Make A as word (2 bytes each item)
+		movq mm4, mm0
+
+		// Zero-ing temp mm3
+		psubsw mm3, mm3
+
+		// Unpacking mm4 from 8 chars to 4 words
+		pcmpgtb mm3, mm4
+		// High-order bytes (Hbw)
+		punpckhbw mm4, mm3
+
+		// Place result to temp mm7
+		movq mm7, mm4
+
+
+
+		// A --- B*C
+		psubsw mm7, mm6
+		movq mm6, mm7
+
+
+
+
+
+		// A-B*C +++ D
+		paddsw mm6, qword ptr[D + 8]
+
+
+
+		// =================================================================================
+
+		movq qword ptr[F], mm5
+		movq qword ptr[F + 8], mm6
 	}
 
+	int w = 6;
+	__int16 result[8] = { 0 };
+
+	for (int i = 0; i < 8; i++) {
+		result[i] = A[i] - (B[i] * C[i]) + D[i];
+	}
+
+	cout << "F[i] = A[i] - (B[i] * C[i]) + D[i]" << endl << endl;
+
+	cout << "Fc: ";
 	for (int i = 0; i < 8; i++)
-		cout << int(a[i]) << "  ";
+		cout << setw(w) << int(result[i]);
 	cout << endl;
 
+	cout << "----------------------------------------------------" << endl;
+
+	
+	cout << "A:  ";
 	for (int i = 0; i < 8; i++)
-		cout << int(b[i]) << "  ";
+		cout << setw(w) << int(A[i]);
 	cout << endl;
 
+	cout << "B:  ";
 	for (int i = 0; i < 8; i++)
-		cout << int(c[i]) << "  ";
+		cout << setw(w) << int(B[i]);
 	cout << endl;
 
+	cout << "C:  ";
 	for (int i = 0; i < 8; i++)
-		cout << int(d[i]) << "  ";
+		cout << setw(w) << int(C[i]);
 	cout << endl;
 
+	cout << "D:  ";
 	for (int i = 0; i < 8; i++)
-		cout << int(f[i]) << "   ";
+		cout << setw(w) << int(D[i]);
+	cout << endl;
+
+	cout << "----------------------------------------------------" << endl;
+
+	
+	cout << "Fa: ";
+	for (int i = 0; i < 8; i++)
+		cout << setw(w) << int(F[i]);
+
+	cout << endl;
 }
+
+
+
+
